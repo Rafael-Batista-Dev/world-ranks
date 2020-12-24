040c5d86376a7990defdb0bfe3455a93a0ddb45f
Rafael-Batist-Dev <dev.rafaelbatista@gmail.com>
2020-12-24 13:28:05 -0300

    consuming api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,19 @@
-import Layout from '../components/Layout/Layout';
-import styles from '../../styles/Home.module.css'
+import Layout from "../components/Layout/Layout";
+import axios from "axios";
 
-export default function Home() {
-  return  <Layout>main</Layout>
+export default function Home({ countries }) {
+  console.log(countries);
+
+  return <Layout>main</Layout>;
 }
+
+export const getServerSideProps = async () => {
+  const res = await axios.get("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
